4c0f538cb5b10293312944837fd47e5d5b609627
Fırat Rüzgar <firatruzgar@Mac.modem.local>
2026-02-11 23:37:28 +0300

    created tokens.css for centralized design variables (colors, spacing, typography, radius, shadows)
    added base and components styling architecture

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Link href="/create-appointment">
+        <button>Go to Appointment page</button>
+      </Link>
+    </div>
+  );
 }
